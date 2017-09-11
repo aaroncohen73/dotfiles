d8a3bd8e4b196452d1acc1e770343184effa698f
Aaron Cohen <aaroncohen73@gmail.com>
2017-09-11 11:04:53 -0700

    Change terminal color scheme, add items to i3status, fix install script

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ mkdir -p ~/.config/terminator
 mkdir -p ~/.vim/bundle
 
 # Install i3 config
-ln -s i3config ~/.config/i3/config
-ln -s i3statusconfig ~/.config/i3status/config
+ln -sf $PWD/i3config ~/.config/i3/config
+ln -sf $PWD/i3statusconfig ~/.config/i3status/config
 
 # Install terminal config/X11 config
-ln -s terminatorconfig ~/.config/terminator/config
-ln -s .Xresources ~/.Xresources
-ln -s .Xmodmap ~/.Xmodmap
+ln -sf $PWD/terminatorconfig ~/.config/terminator/config
+ln -sf $PWD/.Xresources ~/.Xresources
+ln -sf $PWD/.Xmodmap ~/.Xmodmap
 
 # Install vim config
-ln -s .vimrc ~/.vimrc
+ln -sf $PWD/.vimrc ~/.vimrc
 cat vimplugins | xargs -n 1 git clone
 cat vimplugins | sed 'y/\// /' | awk '{print $NF}' | xargs -n 1 -I % mv -f % ~/.vim/bundle/%
